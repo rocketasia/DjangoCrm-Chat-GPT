d25358fe2a70f01d2362825b3603320b0f899047
rocketasia <74543570+rocketasia@users.noreply.github.com>
2024-04-28 13:10:37 +0800

    update new design

--- crm/forms.py
+++ crm/forms.py
@@ -4,4 +4,4 @@ from .models import Customer
 class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields = ['name', 'email']  # List all the fields you want from the model
+        fields = ['firstname', 'surname', 'email', 'phone']  # List all the fields you want from the model
--- crm/models.py
+++ crm/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
 class Customer(models.Model):
-    name = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
